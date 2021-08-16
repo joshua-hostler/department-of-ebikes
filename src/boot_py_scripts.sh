ae88a338c6360ebe3a607a5eb753b7f5b662752d
joshua-hostler <88912191+joshua-hostler@users.noreply.github.com>
2021-08-16 12:24:06 -0800

    Update boot_py_scripts.sh

--- src/boot_py_scripts.sh
+++ src/boot_py_scripts.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-systemctl disable gpsd.socket
 gpsd /dev/serial0 -F /var/run/gpsd.sock
 /home/pi/Documents/ebikes/data-logger/bin/python3.9 /home/pi/Documents/ebikes/source/main.py &
 /home/pi/Documents/ebikes/pico/bin/python3.9 /home/pi/Documents/ebikes/pico/main.py &
